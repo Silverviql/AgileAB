0c59dbc2a2d70e8ba57fa6c5d89be50a065ee8cf
user <user@Mac-mini-user.local>
2017-07-25 16:57:49 +0300

    authentication

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Divider from 'material-ui/Divider';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
@@ -31,63 +31,117 @@ const Styles = {
   logo: {
     width: '50%'
   },
-  input:{
-  //  textAlign: 'center',
+  input: {
+    //  textAlign: 'center',
     сolor: 'rgba(0, 0, 0, 0.9)',
     width: '80%'
   },
-  button:{
+  button: {
     width: '80%'
   }
 };
 
 class LoginPage extends Component {
-  constructor(...args){
+
+  state = {
+    authData: {
+      login: ''
+    },
+    authPass: {
+      password: ''
+    },
+    testDate: {
+      login: "user",
+      password: "user"
+    }
+  };
+
+  constructor(...args) {
     super(...args);
 
     this.onSign = this.onSign.bind(this);
     this.onAuth = this.onAuth.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onPass = this.onPass.bind(this);
   }
 
-  onAuth(){
-    browserHistory.push('/');
+ onAuth() {
+  // console.log(this.state.authData.login );
+  // console.log(this.state.authData.password );
+    if(this.state.authData.login == this.state.testDate.login){
+        browserHistory.push('/');
+    }
+    if(this.state.authPass.password == this.state.testDate.password){
+        browserHistory.push('/');
+    }
+      else {
+        console.log("Неверный логин или пароль")
+      }
   }
 
-  onSign(){
-  browserHistory.push('/sign');
+  onSign() {
+    browserHistory.push('/sign');
   }
 
-
-   handleSubmit(e) {
-    e.preventDefault()
-    this.props.actions.login({name: e.target.elements[0].value})
+  onChange(e) {
+    if(!e.target.value){
+      console.error("no login")
+    }
+    // else { 
+    //   console.log("Authenticated successfully")
+    // }
+    this.setState({
+      authData: {
+        login: e.target.value
+      }
+    })
   }
 
+  onPass(e) {
+      if(!e.target.value){
+      console.error("no pass")
+    }
+      this.setState({
+      authPass: {
+        password: e.target.value
+      }
+    })
+  }
 
-    render(){
-      return (
-        <div style={Styles.main}>
-           <img src={AgileSvg} style={Styles.logo}/> 
-          <TextField hintText="Username"  style={Styles.input} onSubmit={this.handleSubmit} />
-          {/*<TextField hintText="Username"  inputStyle={Styles.input} hintStyle={Styles.input} />*/}
-          <TextField hintText="Password" style={Styles.input}/>
-          <br />
-          <RaisedButton label="LOG IN" primary={true} style={Styles.button} type='submit'/>
-          
-          <FlatButton label="Sign up" primary={true} style={Styles.button} onTouchTap={this.onSign} />
-          
-        </div>
+  render() {
+    return (
+      <div style={Styles.main}>
+        <img src={AgileSvg} style={Styles.logo} />
+        <TextField
+          hintText="Username"
+          style={Styles.input}
+          value={this.state.authData.login} 
+          onChange={this.onChange} />
 
-      )
+        <TextField 
+          hintText="Password" 
+          style={Styles.input}
+        value={this.state.authPass.password} 
+          onChange={this.onPass} 
+          /> 
+
+        <br />
+        <RaisedButton label="LOG IN" primary={true} style={Styles.button} type='submit' onTouchTap={this.onAuth} />
+
+        <FlatButton label="Sign up" primary={true} style={Styles.button} onTouchTap={this.onSign} />
+
+      </div>
+
+    )
   }
 }
-  function mapStateToProps() {
+function mapStateToProps() {
   return {}
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-   actions: bindActionCreators(UserActions, dispatch)
+    actions: bindActionCreators(UserActions, dispatch)
   }
 }
 
